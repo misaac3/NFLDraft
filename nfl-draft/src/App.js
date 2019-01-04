02616aac00690730f48ac2a4676669b632b0adfb
misaac3 <misaac3@wisc.edu>
2019-01-04 17:02:33 -0500

    react initalized

--- nfl-draft/src/App.js
+++ nfl-draft/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            NFL DRAFT
           </a>
         </header>
       </div>
