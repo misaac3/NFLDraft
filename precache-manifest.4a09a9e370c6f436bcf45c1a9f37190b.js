f9d8002d1aefe4d28c8157460644574d37aaf763
misaac3 <misaac3@wisc.edu>
2019-02-04 12:18:04 +0000

    Updates

---
+++ precache-manifest.4a09a9e370c6f436bcf45c1a9f37190b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69c354017f0ef7241a37",
+    "url": "/NFLDraft/static/css/main.334722d3.chunk.css"
+  },
+  {
+    "revision": "69c354017f0ef7241a37",
+    "url": "/NFLDraft/static/js/main.69c35401.chunk.js"
+  },
+  {
+    "revision": "aae6a0ea265ea9995407",
+    "url": "/NFLDraft/static/css/1.53600771.chunk.css"
+  },
+  {
+    "revision": "aae6a0ea265ea9995407",
+    "url": "/NFLDraft/static/js/1.aae6a0ea.chunk.js"
+  },
+  {
+    "revision": "0d50803b41ed079b1a66",
+    "url": "/NFLDraft/static/js/runtime~main.0d50803b.js"
+  },
+  {
+    "revision": "c7098a9a72c320cc2c396143aaea77b8",
+    "url": "/NFLDraft/index.html"
+  }
+];
